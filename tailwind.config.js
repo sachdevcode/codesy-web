0f693b09da446a51a91ed7ad61eaf3c5e23d214e
sachdevcode <karancode.work@gmail.com>
2024-02-07 07:20:39 -0800

    initial-commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const tailwindConfig = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6215F9',
+        secondary: '#FAD707',
+        text: {
+          DEFAULT: '#000000',
+          light: '#FFFFFF',
+        },
+        accent: '#FAD707',
+      },
+      fontFamily: {
+        main: ['Roboto', 'sans-serif'],
+      },
+      fontWeight: {
+        primary: 600,
+        secondary: 400,
+        text: 400,
+        accent: 500,
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default tailwindConfig;
