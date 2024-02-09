79569b7fb9e3e8d4a5b6e67213bc230e12e6e59c
sachdevcode <karancode.work@gmail.com>
2024-02-09 06:22:43 -0800

    --update

--- src/Components/FirstSection.tsx
+++ src/Components/FirstSection.tsx
@@ -3,10 +3,10 @@ import ReuseTile from "../common/ReuseTile";
 
 const FirstSection = () => {
   return (
-    <div className="pt-20">
-      <div className="firstSection">
-        <h1>Hello, We are EpicJam</h1>
-        <h3>
+    <div className="pt-24">
+      <div className="firstSection py-6 px-9">
+        <h1 className="font-bold text-5xl pt-3 max-[800px]:w-auto   text-primary">Hello, We are EpicJam</h1>
+        <h3 className=" text-xl pt-3  max-[800px]:w-auto   text-black">
           A full-service phygital powerhouse guiding businesses towards the
           right solutions.
         </h3>
